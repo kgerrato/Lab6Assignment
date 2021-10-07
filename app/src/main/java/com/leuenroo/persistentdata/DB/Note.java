bb2f6edfbd424450218e96b15997be9d93513914
Moaath Alrajab <moaath@alrajab.net>
2021-10-07 17:11:03 -0400

    Initial Commit

---
+++ app/src/main/java/com/leuenroo/persistentdata/DB/Note.java
@@ -0,0 +1,30 @@
+package com.leuenroo.persistentdata.DB;
+
+
+
+public class Note {
+    private long id;
+    private String note;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String nt) {
+        this.note = nt;
+    }
+
+    // Will be used by the ArrayAdapter in the ListView
+    @Override
+    public String toString() {
+        return note;
+    }
+}
---
+++ app/src/main/java/com/leuenroo/persistentdata/DB/OrganizerStorage.java
@@ -0,0 +1,81 @@
+package com.leuenroo.persistentdata.DB;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrganizerStorage {
+
+    // Database fields
+    private SQLiteDatabase database;
+    private MySQLiteHelper dbHelper;
+    private String[] allColumns = { MySQLiteHelper.COLUMN_ID,
+            MySQLiteHelper.COLUMN_COMMENT };
+
+    public OrganizerStorage(Context context) {
+        dbHelper = new MySQLiteHelper(context);
+    }
+
+    public void open() throws SQLException {
+        database = dbHelper.getWritableDatabase();
+    }
+
+    public void close() {
+        dbHelper.close();
+    }
+
+    public Note createComment(String comment) {
+
+
+        ContentValues values = new ContentValues();
+        values.put(MySQLiteHelper.COLUMN_COMMENT, comment);
+
+        long insertId = database.insert(MySQLiteHelper.TABLE_COMMENTS, null,
+                values);
+
+        Cursor cursor = database.query(MySQLiteHelper.TABLE_COMMENTS,
+                allColumns, MySQLiteHelper.COLUMN_ID + " = " + insertId, null,
+                null, null, null);
+
+        cursor.moveToFirst();
+        Note newComment = cursorToComment(cursor);
+        cursor.close();
+        return newComment;
+    }
+
+    public void deleteNote(Note note) {
+        long id = note.getId();
+        System.out.println("Note deleted with id: " + id);
+        database.delete(MySQLiteHelper.TABLE_COMMENTS, MySQLiteHelper.COLUMN_ID
+                + " = " + id, null);
+    }
+
+    public List<Note> getAllNotes() {
+        List<Note> notes = new ArrayList<Note>();
+
+        Cursor cursor = database.query(MySQLiteHelper.TABLE_COMMENTS,
+                allColumns, null, null, null, null, null);
+
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            Note note = cursorToComment(cursor);
+            notes.add(note);
+            cursor.moveToNext();
+        }
+        // make sure to close the cursor
+        cursor.close();
+        return notes;
+    }
+
+    private Note cursorToComment(Cursor cursor) {
+        Note note = new Note();
+        note.setId(cursor.getLong(0));
+        note.setNote(cursor.getString(1));
+        return note;
+    }
+}
